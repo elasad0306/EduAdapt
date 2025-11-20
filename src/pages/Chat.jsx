3cb7dff03257aed2836639f1da6369209408b361
dalexis <108296@ecole-it.com>
2025-11-20 23:41:23 +0100

    Navbar finit

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,15 +1,32 @@
 import '../App.css'
+import LogoWhite from '../assets/picture/logoEduAdaptWhiteVersion.png'
+import Bell from '../assets/picture/bell.png'
+import Profile from '../assets/picture/profile.jpg'
 
-function App() {
-
-
+function Navbar() {
     return (
-        <>
-            <div className="flex flex-col items-center">
-                    <h1 className="font-bold ">Commencez votre requête</h1>
+        <nav className="flex justify-between items-center bg-gray-800 p-2">
+            <a href="/Chat">
+                <img
+                    src={LogoWhite}
+                    alt="Logo"
+                    className="h-15 cursor-pointer transition-transform duration-200 hover:scale-110"
+                />
+            </a>
+            <div className="flex space-x-6">
+            <img
+                src={Bell}
+                alt="Bouton 1"
+                className="h-10 cursor-pointer transition-transform duration-200 hover:scale-110"
+            />
+            <img
+                src={Profile}
+                alt="Bouton 2"
+                className="h-10 cursor-pointer transition-transform duration-200 hover:scale-110"
+            />
             </div>
-        </>
-    )
+        </nav>
+    );
 }
 
-export default App
+export default Navbar;
