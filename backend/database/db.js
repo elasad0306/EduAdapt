c46c87a105046b8f7f04b7f71f6537f0666e3c9b
dalexis <108296@ecole-it.com>
2025-11-25 16:43:48 +0100

    Ajustement

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 //Connexion à la base de données 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'EduAdapt',
-    password: '',
+    user: 'root',
+    password: 'VnW4z9f6',
     database: 'eduadapt'
 })
 
