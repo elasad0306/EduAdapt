ed9ae6458848ece1b38c0ad81b189fb220d3a744
dalexis <108296@ecole-it.com>
2025-11-25 12:14:23 +0100

    supression information pageProfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from 'react';
+import {useEffect, useState } from 'react';
 import '../App.css'
 import Button from '../Components/Buttons/Button';
 import Footer from '../components/Footer';
@@ -97,52 +97,7 @@ function Profile(){
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col p-6 rounded-lg shadow-lg bg-cover bg-center mt-6 mb-6">
-                    <h2 className="text-center mt-6 text-2xl mb-6"> Historique D'activités</h2>
-                    <table className="border-separate border border-gray-400 mt-4 mb-6">
-                        <thead>
-                            <tr>
-                            <th className="border border-gray-300 px-6 py-3 ">Cours</th>
-                            <th className="border border-gray-300 px-6 py-3 ">Impression</th>
-                            <th className="border border-gray-300 px-6 py-3 ">Score</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <td className="border border-gray-300 px-6 py-3 ">Anglais</td>
-                            <td className="border border-gray-300 px-6 py-3 ">Moyen</td>
-                            <td className="border border-gray-300 px-6 py-3 ">10</td>
-                            </tr>
-                            <tr>
-                            <td className="border border-gray-300 px-6 py-3 ">Mathématiques</td>
-                            <td className="border border-gray-300 px-6 py-3 ">Assez Bien</td>
-                            <td className="border border-gray-300 px-6 py-3 ">15</td>
-                            </tr>
-                            <tr>
-                            <td className="border border-gray-300 px-6 py-3 ">Informatique</td>
-                            <td className="border border-gray-300 px-6 py-3 ">Excellent !</td>
-                            <td className="border border-gray-300 px-6 py-3 ">17</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div>
-                    <h2 className="text-center mt-6 text-2xl mb-6">Demander de l'aide</h2>
-                    <div className="text-center px-60 py-6 bg-cover bg-center bg-sky-100 ">
-                            <p>
-                                Besoin d'un coup de pouce ? Envoyez votre demande et entrez en contact
-                                avec des étudiant·es ou des tuteurs disponibles, localement ou à distance.
-                                <br />
-                                Organisez des sessions d'étude, partagez des ressources et progressez
-                                ensemble — la communauté EduAdapt vous accompagne à chaque étape.
-                            </p>
-                    </div>
-                </div>
-                <div className="flex justify-center mt-4 mb-10">
-                    <a href="/Aide">
-                        <Button NameButton="Demander de l'aide" style="py-2 px-9 cursor-pointer transition duration-300 border border-yellow-300 text-yellow-300  rounded hover:bg-yellow-300 hover:text-white"/>
-                    </a>
-                </div>
+
         <Footer />
         </>
     )
